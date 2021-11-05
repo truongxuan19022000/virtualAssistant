543e6ba802a0142b824a6eae2cd6237af7e84776
Xuân Trường Nguyễn <75318684+truongxuan19022000@users.noreply.github.com>
2021-11-05 15:59:29 +0700

    Update hieu.py

--- hieu.py
+++ hieu.py
@@ -1,14 +1,5 @@
 you ="today"
-<<<<<<< HEAD
-if(you=""):
-    robot_brain="hello baybe"
-elseif(you="cai gi the nay"){
-    robot_brain="cai du ma"
-}
-else{
-    robot_brain="ok"
-}
-=======
+
 if you == "":
     robot_brain="I can't hear you,please say"
 elif you == "hello":
@@ -17,6 +8,6 @@ elif you == "today":
         robot_brain="today 05/11"
 else:
     robot_brain= "i'm fine thank you"
->>>>>>> cf109efa91487b97c1be5fb11078639013794bf1
+
 
 print(robot_brain)
